8e97b94278128d328b4d3e14797d8f082850a0dd
melchr <melchrstmn@gmail.com>
2020-11-01 21:46:18 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :reviews, only: [:index, :create, :show] do
+        resources :comments, only: [:index, :create, :destroy]
+      end
+    end
+  end
 end
