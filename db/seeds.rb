2c6ba32c94794d3fdbbb6e53cac323890d6a61f1
melchr <melchrstmn@gmail.com>
2020-11-08 16:51:25 -0500

    edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,26 @@ Comment.destroy_all
 Review.destroy_all
 
 review1 = Review.create(
-    title: Faker::Music.band + " Album Review",
-    content: Faker::Hipster.paragraph,
+    title: Faker::Music.album + " Album Review",
+    content: Faker::Hipster.paragraph + Faker::Hipster.paragraph,
     genre: Faker::Music.genre,
-    img_url: Faker::LoremPixel.image)
+    img_url: Faker::LoremFlickr.image)
+review2 = Review.create(
+    title: Faker::Music.album + " Album Review",
+    content: Faker::Hipster.paragraph + Faker::Hipster.paragraph,
+    genre: Faker::Music.genre,
+    img_url: Faker::LoremFlickr.image)
+review3 = Review.create(
+    title: Faker::Music.album + " Album Review",
+    content: Faker::Hipster.paragraph + Faker::Hipster.paragraph,
+    genre: Faker::Music.genre,
+    img_url: Faker::LoremFlickr.image)
+review4 = Review.create(
+    title: Faker::Music.album + " Album Review",
+    content: Faker::Hipster.paragraph + Faker::Hipster.paragraph,
+    genre: Faker::Music.genre,
+    img_url: Faker::LoremFlickr.image)
 
 
-    comment1 = Comment.create(content: Faker::Hipster.paragraph, review_id: review1.id)
+comment1 = Comment.create(content: Faker::Hipster.paragraph, review_id: review1.id)
+comment2 = Comment.create(content: Faker::Hipster.paragraph, review_id: review2.id)
